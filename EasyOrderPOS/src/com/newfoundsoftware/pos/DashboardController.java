75703bf340903d530451ab438a1e4cec0550aeed
florivula <fv57184@ubt-uni.net>
2023-04-16 00:30:46 +0200

    Database and Dashboard
    Database and a simple version of the dashboard have been created with a login validation.

---
+++ EasyOrderPOS/src/com/newfoundsoftware/pos/DashboardController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.newfoundsoftware.pos;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Flori Vula
+ */
+public class DashboardController implements Initializable {
+
+    @FXML
+    private Label lblUsername;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void setUsername(String username){
+        lblUsername.setText(username);
+    }
+}
---
+++ EasyOrderPOS/src/com/newfoundsoftware/pos/JdbcDao.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.newfoundsoftware.pos;
+
+/**
+ *
+ * @author Flori Vula
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+public class JdbcDao {
+    
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/posjavafx?useSSL=false";
+    private static final String DATABASE_USERNAME = "root";
+    private static final String DATABASE_PASSWORD = "";
+    private static final String SELECT_QUERY = "SELECT * FROM users WHERE username = ? AND password = ?"; //query per mi kqyr a ekziston useri ne database (selekton te gjithe userat ne databazse)
+    
+    public boolean validate(String username, String password){
+        //hapi 1: connection
+        try{
+            
+            Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            //hapi 2: krijimi i nje statement me perdorimin e objektit per konektim
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            
+            System.out.println(preparedStatement);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }
+            
+        }catch(SQLException e){
+            //per printim te sql exception
+            printSQLException(e);
+        }
+        return false;
+    }
+    
+    public static void printSQLException(SQLException ex){
+        //per mi pas debugging ma t'leht gjate vazhdimit te projektit
+        for(Throwable e: ex){
+            if(ex instanceof SQLException){
+                e.printStackTrace(System.err);
+                System.out.println("SQLState: " + ((SQLException)e).getSQLState());
+                System.out.println("Error Code: " + ((SQLException)e).getErrorCode());
+                System.out.println("Message: " + ex.getMessage());
+                Throwable t = ex.getCause();
+                while(t != null){
+                    System.out.println("Cause: " + t);
+                    e = t.getCause();
+                }
+            }
+        }
+    }
+}
