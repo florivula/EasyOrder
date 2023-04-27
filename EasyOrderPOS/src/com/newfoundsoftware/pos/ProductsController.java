cbe21672331315c3a8d0db0e2f565f9d666aa82a
Olti Dinarama <125982441+cudeey@users.noreply.github.com>
2023-04-27 23:09:40 +0200

    Manage Categories Window
    onclick modal window open for category managing

--- EasyOrderPOS/src/com/newfoundsoftware/pos/ProductsController.java
+++ EasyOrderPOS/src/com/newfoundsoftware/pos/ProductsController.java
@@ -4,6 +4,7 @@
  */
 package com.newfoundsoftware.pos;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -13,7 +14,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
@@ -21,6 +25,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -66,6 +72,11 @@ public class ProductsController implements Initializable {
     private TableColumn<Products, String> colCategory;
     @FXML
     private TableColumn<Products, String> colStatus;
+    
+    Scene fxmlFile;
+    Parent root;
+    Stage window;
+
 
     /**
      * Initializes the controller class.
@@ -145,6 +156,28 @@ public class ProductsController implements Initializable {
         }catch(Exception ex){
             System.out.println("error while inserting record.");
             ex.printStackTrace();
+        }
+    }
+    
+    private void openModalWindow(String resource, String title) throws IOException{
+        root = FXMLLoader.load(getClass().getResource(resource));
+        fxmlFile = new Scene(root);
+        window = new Stage();
+        window.setScene(fxmlFile);
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setAlwaysOnTop(true);
+        window.setIconified(false);
+        //window.initStyle(StageStyle.UNDECORATED);
+        window.setTitle(title);
+        window.showAndWait();
+    }
+
+    @FXML
+    private void actionAddCategory(ActionEvent event) {
+        try{
+            openModalWindow("Category.fxml", "Manage Categories");
+        }catch(Exception ex){
+            
         }
     }
     
