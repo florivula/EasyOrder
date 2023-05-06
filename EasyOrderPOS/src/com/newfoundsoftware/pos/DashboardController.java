d393645416f5f777830905455dd1e69f69e2e2de
florivula <fv57184@ubt-uni.net>
2023-05-07 00:52:44 +0200

    Shortcut Keys
    Added the ability to access certain dashboard buttons with keys.

--- EasyOrderPOS/src/com/newfoundsoftware/pos/DashboardController.java
+++ EasyOrderPOS/src/com/newfoundsoftware/pos/DashboardController.java
@@ -15,6 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -30,43 +31,43 @@ public class DashboardController implements Initializable {
     private Label lblUsername;
     @FXML
     private Button btnManageTable;
-    
+
     Scene fxmlFile;
     Parent root;
     Stage window;
 
     private static Stage pStage;
-    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
-    public void setUsername(String username){
+    }
+
+    public void setUsername(String username) {
         lblUsername.setText(username);
     }
-    
-    private void setPrimaryStage(Stage pStage){
-        DashboardController.pStage = pStage; 
+
+    private void setPrimaryStage(Stage pStage) {
+        DashboardController.pStage = pStage;
     }
-    
-    public static Stage getPrimaryStage(){
+
+    public static Stage getPrimaryStage() {
         return pStage;
     }
 
     @FXML
-    private void manageTable(ActionEvent event){
-        try{
-            openModalWindow("Tables.fxml","Manage Tables");
-        }catch(Exception ex){
-            
+    private void manageTable(ActionEvent event) {
+        try {
+            openModalWindow("Tables.fxml", "Manage Tables");
+        } catch (Exception ex) {
+
         }
     }
-    
-    private void openModalWindow(String resource, String title) throws IOException{
+
+    private void openModalWindow(String resource, String title) throws IOException {
         root = FXMLLoader.load(getClass().getResource(resource));
         fxmlFile = new Scene(root);
         window = new Stage();
@@ -82,10 +83,46 @@ public class DashboardController implements Initializable {
 
     @FXML
     private void actionManageProduct(ActionEvent event) {
-        try{
-            openModalWindow("Products.fxml","Manage Products");
-        }catch(Exception ex){
-            
+        try {
+            openModalWindow("Products.fxml", "Manage Products");
+        } catch (Exception ex) {
+
+        }
+    }
+
+    @FXML
+    private void handleKeyPressed(KeyEvent event) {
+        System.out.println("Key pressed: " + event.getCode());
+        switch (event.getCode()) {
+            case F1:
+                System.out.println("New Order");
+                break;
+            case F2:
+                System.out.println("Payment");
+                break;
+            case F3:
+                System.out.println("Cancel Order");
+                break;
+            case F4:
+                System.out.println("Manage Products");
+                try {
+                    openModalWindow("Products.fxml", "Manage Products");
+                } catch (Exception ex) {
+                }
+                break;
+            case F5: 
+                System.out.println("Manage Table");
+                try {
+                    openModalWindow("Tables.fxml", "Manage Tables");
+                } catch (Exception ex) {
+                }
+                break;
+            case F6:
+                System.out.println("Sales Report");
+                break;
+            case F7:
+                System.out.println("Logout");
+                break;
         }
     }
 }
