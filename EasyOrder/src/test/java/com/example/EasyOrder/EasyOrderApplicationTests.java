8ff3cfc1cb6891f675d6533bb0a4dace2d406400
zanzinjo <shpendzani@gmail.com>
2023-06-09 11:05:54 +0200

    Java Spring Boot - Environment
    Setup for Java Spring Boot. Placed html files inside the "templates" directory and all other files under "static" directory.
    Mirmëngjesi trrima.

---
+++ EasyOrder/src/test/java/com/example/EasyOrder/EasyOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EasyOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
